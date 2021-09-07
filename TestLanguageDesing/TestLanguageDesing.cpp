d9949ced05955ccbe156b8ef077984913a23fc8f
Patrick Kranz <patrick-kranz@live.de>
2021-09-07 20:52:25 +0200

    Add project files.

---
+++ TestLanguageDesing/TestLanguageDesing.cpp
@@ -0,0 +1,96 @@
+﻿// TestLanguageDesing.cpp : Defines the entry point for the application.
+//
+
+#include "TestLanguageDesing.h"
+
+using namespace std;
+
+
+
+ObjectHandler mainHandler;
+
+ObjectMetadata::ObjectMetadata()
+{
+}
+
+bool ObjectMetadata::isArray() const {
+	return this->ArrayLength > 0;
+}
+
+bool ObjectMetadata::isFree() const {
+	return this->Id < 0;
+}
+
+
+
+int16_t  Reference::getId()  const {
+	return this->Id & INT_FAST16_MAX;
+}
+bool Reference::isOwner() const {
+	return 	this->Id < 0;
+}
+
+bool Reference::isValid() const {
+	ObjectMetadata metadata;
+	return mainHandler.GetMetadata(*this, metadata) == GetMetadataStatus::Success;
+}
+
+
+
+ObjectHandler::ObjectHandler() {
+
+}
+
+GetMetadataStatus ObjectHandler::GetMetadata(const MetadataPointer& pointer, ObjectMetadata& metadata) {
+	if (data.size() >= pointer)
+		return GetMetadataStatus::Deleted;
+	metadata = data.at(pointer);
+	return GetMetadataStatus::Success;
+}
+GetMetadataStatus ObjectHandler::GetMetadata(_In_ const Reference& pointer, _Out_  ObjectMetadata& metadata) {
+	ObjectMetadata result;
+	if (GetMetadata(pointer.metadata, result) != GetMetadataStatus::Success)
+		return GetMetadataStatus::Deleted;
+	if (result.Id == pointer.getId()) {
+		metadata = result;
+		return GetMetadataStatus::Success;
+	}
+	return GetMetadataStatus::Deleted;
+}
+
+DeleteStatus ObjectHandler::Delete(_In_ const Reference& pointer) {
+	if (!pointer.isOwner())
+		return  DeleteStatus::NotOwner;
+
+	ObjectMetadata metadata;
+	if (GetMetadata(pointer, metadata) != GetMetadataStatus::Success) {
+		return DeleteStatus::NotValid;
+	}
+
+	metadata.Id = -metadata.Id;
+	return DeleteStatus::Success;
+}
+
+bool ObjectHandler::LoadType(_In_ TypeData& t, _Out_ ObjectMetadata& metadata) {
+	
+	metadata = {};
+	metadata.Type = 
+
+	return true;
+}
+
+bool ObjectHandler::AllocateInstance(_In_ ObjectMetadata& t, _Out_ ObjectMetadata& metadata) {
+	metadata = {};
+	return true;
+}
+
+
+
+
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ TestLanguageDesing/TestLanguageDesing.h
@@ -0,0 +1,114 @@
+﻿// TestLanguageDesing.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using  MetadataPointer = uint16_t;
+
+
+
+struct ObjectMetadata {
+	uint16_t OwnerThread; // 0 is shared
+	int16_t Id; // negative is deleted
+	uint16_t ArrayLength; // zero is no array
+	MetadataPointer Type;
+	void* object;
+	uint16_t ObjectSize; // 
+
+public:
+
+	ObjectMetadata();
+
+	bool isArray() const;
+
+	bool isFree() const;
+};
+
+struct Reference
+{
+private:
+	int16_t Id;
+
+public:
+	MetadataPointer metadata;
+
+	int16_t  getId()  const;
+	bool isOwner() const;
+
+	bool isValid() const;
+
+};
+
+enum class GetMetadataStatus
+{
+	Success,
+	Deleted,
+};
+
+enum class DeleteStatus
+{
+	Success,
+	NotOwner,
+	NotValid,
+};
+
+
+
+enum class Instructions : uint_fast8_t
+{
+
+};
+
+
+struct FieldData
+{
+
+};
+
+
+struct MethodData
+{
+	std::vector<Instructions> instructions;
+};
+
+
+struct TypeData {
+
+public:
+	wstring name;
+	std::vector< FieldData> fields;
+	std::vector< MethodData> methods;
+};
+
+
+
+
+struct ObjectHandler {
+
+private:
+	std::vector<ObjectMetadata> data;
+
+public:
+
+	ObjectHandler();
+
+	_Check_return_
+		_Success_(return == GetMetadataStatus::Success)
+		GetMetadataStatus GetMetadata(_In_ const MetadataPointer& pointer, _Out_  ObjectMetadata& metadata);
+	_Check_return_
+		_Success_(return == GetMetadataStatus::Success)
+		GetMetadataStatus GetMetadata(_In_ const Reference& pointer, _Out_  ObjectMetadata& metadata);
+
+	DeleteStatus Delete(_In_ const Reference& pointer);
+
+	bool LoadType(_In_ TypeData& t, _Out_ ObjectMetadata& metadata);
+
+	bool AllocateInstance(_In_ ObjectMetadata& t, _Out_ ObjectMetadata& metadata);
+
+};
+
+
+// TODO: Reference additional headers your program requires here.
